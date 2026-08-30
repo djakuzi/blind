b407f1e22f27438b2c5e199ecc1412e3027bb9e8
djakuzi <djakuzi@djakuzis-MacBook-Air.local>
2026-08-30 17:16:38 +0300

    modify: change docs

---
+++ src/app/router/constants/route.const.ts
@@ -0,0 +1,14 @@
+export const KEY_ROUTE = {
+    menu: {
+        index: 'menu',
+    },
+    settings: {
+        index: 'settings',
+    },
+    game: {
+        index: 'game',
+    }
+}
+
+
+export type tKeyRoute = typeof KEY_ROUTE;
