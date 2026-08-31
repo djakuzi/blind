87be34f119552a2401ce7d487ba90f6124672ce0
djakuzi <matvey.ananev.02@mail.ru>
2026-09-01 02:04:24 +0300

    modify: move files vue

--- src/app/styles/contracts/base.ts
+++ src/app/styles/contracts/base.ts
@@ -1,5 +1,5 @@
 export const BASE_ADAPTIVE_VARIANTS = ['small', 'middle', 'big'] as const
 export const BASE_SIZE_VARIANTS = BASE_ADAPTIVE_VARIANTS
 
-export type BaseAdaptiveVariant = (typeof BASE_ADAPTIVE_VARIANTS)[number]
-export type BaseSizeVariant = (typeof BASE_SIZE_VARIANTS)[number]
+export type tBaseAdaptiveVariant = (typeof BASE_ADAPTIVE_VARIANTS)[number]
+export type tBaseSizeVariant = (typeof BASE_SIZE_VARIANTS)[number]
