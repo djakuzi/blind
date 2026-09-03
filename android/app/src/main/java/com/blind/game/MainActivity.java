75d77e45537bbc996986cfd98fe298f663da06bb
djakuzi <matvey.ananev.02@mail.ru>
2026-09-03 04:20:35 +0300

    feat: add new action

--- android/app/src/main/java/com/blind/game/MainActivity.java
+++ android/app/src/main/java/com/blind/game/MainActivity.java
@@ -1,5 +1,39 @@
 package com.blind.game;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        enableFullscreen();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) {
+            enableFullscreen();
+        }
+    }
+
+    private void enableFullscreen() {
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        );
+    }
+}
