77c52f57c54dc89df0d1b4cb71aa47d98d5cfd28
djakuzi <matvey.ananev.02@mail.ru>
2026-09-05 02:47:46 +0300

    modify: change src

--- src/app/view/menu/ViewMenu.vue
+++ src/app/view/menu/ViewMenu.vue
@@ -3,6 +3,8 @@ import { useRouter } from 'vue-router';
 
 import ViewLayout from '@/app/layouts/components/view/ViewLayout.vue';
 import { KEY_ROUTE } from '@/app/router/constants/route.const';
+import AppFlex from '@/app/shared/components/atoms/block/AppFlex.vue';
+import AppPosition from '@/app/shared/components/atoms/layer/AppPosition.vue';
 import AppButtonHold from '@/app/shared/components/ui/button/AppButtonHold.vue';
 import AppHoldHint from '@/app/shared/components/ui/hint/AppHoldHint.vue';
 import AppLogo from '@/app/shared/components/ui/logo/AppLogo.vue';
@@ -26,7 +28,13 @@ function handleSettings() {
     justify="center"
     padding="none"
   >
-    <div class="view-menu__content">
+    <AppFlex
+      class="view-menu__content"
+      direction="column"
+      align="center"
+      width="100%"
+      max-width="46rem"
+    >
       <AppLogo
         class="view-menu__logo"
         size="big"
@@ -34,7 +42,13 @@ function handleSettings() {
         width="41rem"
       />
 
-      <div class="view-menu__nav">
+      <AppFlex
+        class="view-menu__nav"
+        direction="column"
+        :gap="8"
+        width="100%"
+        max-width="42rem"
+      >
         <AppButtonHold
           :actions="{ complete: handlePlay }"
           text="Играть"
@@ -50,15 +64,20 @@ function handleSettings() {
           max-width="100%"
           size="big"
         />
-      </div>
+      </AppFlex>
 
       <AppHoldHint class="view-menu__hint" />
-    </div>
+    </AppFlex>
 
-    <AppVersion
-      class="view-menu__version"
-      size="big"
-    />
+    <AppPosition
+      type="absolute"
+      :position="{
+        right: 'horizontal',
+        bottom: 'vertical',
+      }"
+    >
+      <AppVersion size="big" />
+    </AppPosition>
   </ViewLayout>
 </template>
 
@@ -68,10 +87,6 @@ function handleSettings() {
 }
 
 .view-menu__content {
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  width: min(100%, 46rem);
   min-width: 0;
   padding: var(--app-space-8) var(--app-space-4);
 }
@@ -80,21 +95,8 @@ function handleSettings() {
   margin-bottom: var(--app-space-20);
 }
 
-.view-menu__nav {
-  display: flex;
-  flex-direction: column;
-  width: min(100%, 42rem);
-  gap: var(--app-space-8);
-}
-
 .view-menu__hint {
   margin-top: var(--app-space-16);
-}
-
-.view-menu__version {
-  position: absolute;
-  right: max(var(--app-space-6), var(--app-safe-area-right));
-  bottom: max(var(--app-space-5), var(--app-safe-area-bottom));
 }
 
 @media (max-height: 42rem) {
