c8d6fade7636360d4cabb2b86b3333f5730d6b8a
djakuzi <matvey.ananev.02@mail.ru>
2026-09-06 23:41:13 +0300

    modify: changes rc

--- src/app/shared/components/ui/hint/AppHoldHint.vue
+++ src/app/shared/components/ui/hint/AppHoldHint.vue
@@ -23,7 +23,7 @@ defineProps<Props>();
 .app-hold-hint {
   display: inline-block;
   color: var(--app-color-text-secondary);
-  font-size: var(--app-font-size-md);
+  font-size: var(--app-font-size-xl);
   font-weight: var(--app-font-weight-medium);
   line-height: var(--app-line-height-control);
   letter-spacing: var(--app-letter-spacing-wider);
--- src/app/view/preGame/ViewGameMode.vue
+++ src/app/view/preGame/ViewGameMode.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import WidgetSliderGameMode from '@/app/features/game/widgets/WidgetSliderGameMode.vue';
 import ViewLayout from '@/app/layouts/components/view/ViewLayout.vue';
+import AppHoldHint from '@/app/shared/components/ui/hint/AppHoldHint.vue';
+import AppFlex from '@/app/shared/components/atoms/block/AppFlex.vue';
+
 </script>
 
 <template>
@@ -11,7 +14,16 @@ import ViewLayout from '@/app/layouts/components/view/ViewLayout.vue';
     padding="none"
     overflow="hidden"
   >
-    <WidgetSliderGameMode />
+    <AppFlex
+      direction="column"
+      align="center"
+      width="100%"
+      :gap="16"
+    >
+      <WidgetSliderGameMode />
+
+      <AppHoldHint text="Удерживайте, чтобы начать" />
+    </AppFlex>
   </ViewLayout>
 </template>
 
