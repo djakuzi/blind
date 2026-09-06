0727ac990699f1ed9e0d5316faaa8f603daecc67
djakuzi <matvey.ananev.02@mail.ru>
2026-09-06 20:56:14 +0300

    modify: change src

--- src/app/features/game/constants/gameMode.const.ts
+++ src/app/features/game/constants/gameMode.const.ts
@@ -1,8 +1,11 @@
 import type { tOptionGameMode } from "@/game/types/gameMode.types";
+import imgDuel from "@/assets/images/gameMode/duel.png";
+import imgCombat from "@/assets/images/gameMode/combat.png";
 
 export interface DataGameMode {
     title: string;
     description: string;
+    img: string;
     options: tOptionGameMode;
 }
 
@@ -10,20 +13,22 @@ export const GAME_MODE: Record<string, DataGameMode> = {
   DUEL: {
     title: 'DUEL',
     description: 'Одно попадание — одна жизнь',
+    img: imgDuel,
     options: {
       players: 2,
       rounds: 3,
       winCondition: 'single-hit',
-    }
+    },
   },
   COMBAT: {
     title: 'COMBAT',
     description: 'Сражайся, пока здоровье не закончится',
+    img: imgCombat,
     options: {
       players: 2,
       rounds: 3,
       winCondition: 'health',
-    }
+    },
   },
 } as const;
 
