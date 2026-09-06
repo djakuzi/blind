20e15bad4abbaa997a79df5816efd39c9eb1777e
djakuzi <matvey.ananev.02@mail.ru>
2026-09-06 19:33:25 +0300

    modify: change src

---
+++ src/app/view/preGame/ViewGameMode.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ViewLayout from '@/app/layouts/components/view/ViewLayout.vue';
+
+</script>
+
+<template>
+  <ViewLayout>
+    fgdfg
+  </ViewLayout>
+</template>
+
+<style scoped>
+</style>
