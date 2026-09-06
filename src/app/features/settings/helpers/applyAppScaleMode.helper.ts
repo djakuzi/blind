5a8246370231d897964aa89540735b3fb69f1a6d
djakuzi <matvey.ananev.02@mail.ru>
2026-09-06 19:16:53 +0300

    modify: change src

--- src/app/features/settings/helpers/applyAppScaleMode.helper.ts
+++ src/app/features/settings/helpers/applyAppScaleMode.helper.ts
@@ -1,43 +1,16 @@
 import {
   APP_SCALE_CSS_VARIABLE_NAME,
   APP_SCALE_MODE,
-  APP_SCALE_SYSTEM_MODE,
-  resolveNearestAppScalePresetMode,
-  type tAppScaleMode,
   type tAppScalePresetMode,
 } from '@/app/styles/contracts/appScale.contract';
 
 import { DomProperty } from '@/core/dom/property';
-import { ToolSystem } from '@/core/tool/system';
 
-async function resolveAppScalePresetMode(
-  appScaleMode: tAppScaleMode,
-): Promise<tAppScalePresetMode> {
-  if (appScaleMode !== APP_SCALE_SYSTEM_MODE) {
-    return appScaleMode;
-  }
-
-  const { value } =
-    await ToolSystem.getSystemScale();
-
-  return resolveNearestAppScalePresetMode(
-    value,
-  );
-}
-
-export async function applyAppScaleMode(
-  appScaleMode: tAppScaleMode,
+export function applyAppScaleMode(
+  appScaleMode: tAppScalePresetMode,
 ) {
-  const resolvedAppScaleMode =
-    await resolveAppScalePresetMode(
-      appScaleMode,
-    );
-
-  const appScaleValue =
-    APP_SCALE_MODE[resolvedAppScaleMode];
-
   DomProperty.setProperty(
     APP_SCALE_CSS_VARIABLE_NAME,
-    String(appScaleValue),
+    String(APP_SCALE_MODE[appScaleMode]),
   );
 }
