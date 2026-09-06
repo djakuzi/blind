bc84f2027cd92c13813ef169359c8709ec04f858
djakuzi <matvey.ananev.02@mail.ru>
2026-09-06 23:19:12 +0300

    modify: change src

---
+++ src/app/view/preGame/ViewGameTypeConnection.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import ViewLayout from '@/app/layouts/components/view/ViewLayout.vue';
+</script>
+
+<template>
+  <ViewLayout
+    align="center"
+    justify="center"
+  >
+    TYPE CONNECTION
+  </ViewLayout>
+</template>
