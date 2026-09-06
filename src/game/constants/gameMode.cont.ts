248e429f8b8623e8a03e4e0f02dcdbec0d277111
djakuzi <matvey.ananev.02@mail.ru>
2026-09-06 20:26:42 +0300

    modify: change src

--- src/game/constants/gameMode.cont.ts
+++ src/game/constants/gameMode.cont.ts
@@ -1,19 +1,27 @@
+export const GAME_MODE_WIN_CONDITION = {
+  SINGLE_HIT: 'single-hit',
+  HEALTH: 'health',
+} as const;
+
 export const GAME_MODE = {
-  COMBAT: {
-    title: 'COMBAT',
-    description: 'Сражайся, пока здоровье не закончится',
-    players: 2,
-    rounds: 3,
-    winCondition: 'health',
-  },
   DUEL: {
     title: 'DUEL',
     description: 'Одно попадание — одна жизнь',
     players: 2,
     rounds: 3,
-    winCondition: 'single-hit',
-  }
+    winCondition: GAME_MODE_WIN_CONDITION.SINGLE_HIT,
+  },
+
+  COMBAT: {
+    title: 'COMBAT',
+    description: 'Сражайся, пока здоровье не закончится',
+    players: 2,
+    rounds: 3,
+    winCondition: GAME_MODE_WIN_CONDITION.HEALTH,
+  },
 } as const;
 
 export type tGameMode = typeof GAME_MODE;
 export type tKeyGameMode = keyof typeof GAME_MODE;
+export type tGameModeWinCondition = typeof GAME_MODE_WIN_CONDITION;
+export type tKeyGameModeWinCondition = keyof typeof GAME_MODE_WIN_CONDITION;
