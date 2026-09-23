f7203916eaf87e76a06c22bbec6a5aca0a2ef451
djakuzi <matvey.ananev.02@mail.ru>
2026-09-23 20:51:20 +0300

    modify: change lang

---
+++ src/app/domain/lang/languages/en.ts
@@ -0,0 +1,20 @@
+import type { Locale } from '../locale';
+
+export const localeEn = {
+    views: {
+        menu: {
+            index: {
+                play: 'Play',
+                settings: 'Settings',
+            },
+
+            settings: {
+                index: {
+                    changeTheme: 'Change theme',
+                },
+            },
+        },
+    },
+
+    modals: {},
+} satisfies Locale;
---
+++ src/app/domain/lang/languages/ru.ts
@@ -0,0 +1,20 @@
+import type { Locale } from '../locale';
+
+export const localeRu = {
+    views: {
+        menu: {
+            index: {
+                play: 'Играть',
+                settings: 'Настройки',
+            },
+
+            settings: {
+                index: {
+                    changeTheme: 'Сменить тему',
+                },
+            },
+        },
+    },
+
+    modals: {},
+} satisfies Locale;
