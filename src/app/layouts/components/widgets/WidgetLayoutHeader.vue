2ed3ab3049c5fd81a5b7e2774306ee06bb86d8fd
Matvey <145272981+djakuzi@users.noreply.github.com>
2026-09-24 22:33:05 +0300

    refactor: use locale feature composables

--- src/app/layouts/components/widgets/WidgetLayoutHeader.vue
+++ src/app/layouts/components/widgets/WidgetLayoutHeader.vue
@@ -1,21 +1,15 @@
 <script setup lang="ts">
-import { computed } from 'vue';
 import { useRouter } from 'vue-router';
+import { useLocale } from '@/app/features/locale/composables/useLocale';
 import AppFlex from '@/app/shared/components/atoms/block/AppFlex.vue';
 import AppButtonIcon from '@/app/shared/components/ui/button/AppButtonIcon.vue';
 import AppLogo from '@/app/shared/components/ui/logo/AppLogo.vue';
-import { useLanguageStore } from '@/app/stores/language/language.store';
 
 const router = useRouter();
-const languageStore = useLanguageStore();
 
-const commonLocale = computed(() => {
-  if (!languageStore.locale) {
-    throw new Error('Locale is not initialized');
-  }
-
-  return languageStore.locale.common;
-});
+const commonLocale = useLocale(
+  locale => locale.common,
+);
 
 function handleBack() {
   router.back();
