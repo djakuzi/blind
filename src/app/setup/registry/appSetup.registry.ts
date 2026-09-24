32ef48ae0ebf082846a3326c128ea19f36225004
Matvey <145272981+djakuzi@users.noreply.github.com>
2026-09-24 23:59:56 +0300

    refactor: add structured app setup modules

---
+++ src/app/setup/registry/appSetup.registry.ts
@@ -0,0 +1,16 @@
+import type { Pinia } from 'pinia';
+import { createLanguageSetup } from '../modules/language.setup';
+import { createScaleSetup } from '../modules/scale.setup';
+import { createThemeSetup } from '../modules/theme.setup';
+import { createViewSetup } from '../modules/view.setup';
+
+export function createAppSetupRegistry(
+  pinia: Pinia,
+) {
+  return [
+    createViewSetup(),
+    createLanguageSetup(pinia),
+    createScaleSetup(pinia),
+    createThemeSetup(pinia),
+  ];
+}
