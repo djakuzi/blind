99d7ba1b87560b72e713b6ef2d42093b86863778
Matvey <145272981+djakuzi@users.noreply.github.com>
2026-09-24 23:59:48 +0300

    refactor: add structured app setup modules

---
+++ src/app/setup/modules/scale.setup.ts
@@ -0,0 +1,19 @@
+import type { Pinia } from 'pinia';
+import { useAppScaleSetup } from '@/app/features/settings/composables/useAppScaleSetup';
+import type { iAppSetup } from '../core/setup.type';
+
+export function createScaleSetup(
+  pinia: Pinia,
+): iAppSetup {
+  const {
+    setupAppScale,
+  } = useAppScaleSetup(pinia);
+
+  return {
+    key: 'scale',
+
+    async preMount() {
+      await setupAppScale();
+    },
+  };
+}
