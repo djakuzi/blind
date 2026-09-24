8898406d3d4132b0f9ee9b55d8257f7f193aaa63
Matvey <145272981+djakuzi@users.noreply.github.com>
2026-09-24 22:24:23 +0300

    refactor: add language display name helper

---
+++ src/app/shared/lib/text/modules/language.modules.ts
@@ -0,0 +1,24 @@
+function getLanguageDisplayName(
+  languageCode: string,
+  displayLanguageCode: string,
+): string | null {
+  if (typeof Intl.DisplayNames === 'undefined') {
+    return null;
+  }
+
+  try {
+    return new Intl.DisplayNames(
+      [displayLanguageCode],
+      {
+        type: 'language',
+      },
+    ).of(languageCode) ?? null;
+  }
+  catch {
+    return null;
+  }
+}
+
+export const moduleLanguage = {
+  getLanguageDisplayName,
+};
