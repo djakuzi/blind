b1cf3b6295847cf899fc09c6ef5a5fe1df3e5bca
Matvey <145272981+djakuzi@users.noreply.github.com>
2026-09-24 23:59:26 +0300

    refactor: add app setup lifecycle core

---
+++ src/app/setup/composables/useAppSetup.ts
@@ -0,0 +1,17 @@
+import { computed } from 'vue';
+import { appSetupState } from '../core/setup.state';
+
+export function useAppSetup() {
+  const isReady = computed(() =>
+    appSetupState.isReady,
+  );
+
+  const isRunning = computed(() =>
+    appSetupState.isRunning,
+  );
+
+  return {
+    isReady,
+    isRunning,
+  };
+}
